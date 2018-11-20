3f21e30080c048dd6b53c5dab5d8218482fa460b
xiaoming <1209819655@qq.com>
2018-11-20 18:18:42 +0800

    页面跳转

--- src/main/java/com/chatRobot/controller/UserLoginController.java
+++ src/main/java/com/chatRobot/controller/UserLoginController.java
@@ -6,6 +6,7 @@ import com.chatRobot.model.User;
 import com.chatRobot.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +21,7 @@ public class UserLoginController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public Msg userLogin(String email, String password){
+    public Msg userLogin(String email, String password, Model model){
         System.out.println("获取值:"+ email);
 
         List<User> userList = UserService.selectByEmail(email);
@@ -28,11 +29,27 @@ public class UserLoginController {
             return Msg.fail().add("message","该邮箱未注册！");
         }
         if(password.equals(userList.get(0).getUserPassword())){
+            model.addAttribute("User",userList.get(0));
             return Msg.success();
         }else{
             return Msg.fail().add("message","密码错误！");
         }
-        //System.out.println(user.toString());
     }
 
+    @RequestMapping("/index")
+    public String userForIndex(String email,String password,Model model){
+        List<User> userList = UserService.selectByEmail(email);
+        if(userList.isEmpty()){
+            model.addAttribute("message","该邮箱未注册!");
+            return "login"; // 直接带参数的网页跳转
+        }
+        if(password.equals(userList.get(0).getUserPassword())){
+            model.addAttribute("message","登陆成功！");
+            model.addAttribute("User",userList.get(0));
+            return "index";
+        }else{
+            model.addAttribute("message","密码错误！");
+            return "login";
+        }
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sun xiaoming
+  Date: 2018/11/20
+  Time: 14:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>this is the web-inf views login.jsp</h1>
+</body>
+</html>
